39b118b1a36d52fd18f11957a4cf0921ad8413dc
AndreSerafin <andreserafim11@gmail.com>
2022-04-02 14:38:57 -0300

    commit 2

--- ExerciciosUri/src/uri1015.java
+++ ExerciciosUri/src/uri1015.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 mostrando 4 casas decimais após a vírgula, segundo a fórmula:*/
 public class uri1015 {
     public static void main(String[] args){
-        int x1,y1,x2,y2;
-        double dist;
+        
+        double dist,x1,y1,x2,y2;
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         
-        x1 = sc.nextInt();
-        y1 = sc.nextInt();
-        x2 = sc.nextInt();
-        y2 = sc.nextInt();
+        x1 = sc.nextDouble();
+        y1 = sc.nextDouble();
+        x2 = sc.nextDouble();
+        y2 = sc.nextDouble();
         
         dist = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 
-        System.out.printf("%.4f",dist);
+        System.out.printf("%.4f\n",dist);
 
         sc.close();
         
--- ExerciciosUri/src/uri1016.java
+++ ExerciciosUri/src/uri1016.java
@@ -1,23 +1,19 @@
 import java.util.Locale; 
 import java.util.Scanner;
-/*Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, 
-mostrando 4 casas decimais após a vírgula, segundo a fórmula:*/
-public class uri1015 {
+/*Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 
+90 Km/h.
+Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
+Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.*/
+public class uri1016 {
     public static void main(String[] args){
-        int x1,y1,x2,y2;
-        double dist;
-
+        
+        int horas,velMed;
+        double dist,kmLitro;
+        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        
-        x1 = sc.nextInt();
-        y1 = sc.nextInt();
-        x2 = sc.nextInt();
-        y2 = sc.nextInt();
-        
-        dist = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 
-        System.out.printf("%.4f",dist);
+        
 
         sc.close();
         
