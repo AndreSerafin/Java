e3da7da24e8faeca2a866569a1e36d2b79d30e32
AndreSerafin <andreserafim11@gmail.com>
2022-04-08 17:19:45 -0300

    exercicios sequencial

--- ExerciciosUri/src/uri1154.java
+++ ExerciciosUri/src/uri1154.java
@@ -1,17 +1,33 @@
 import java.util.Locale;
 import java.util.Scanner;
-/*Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. 
-Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 4.Fim). 
-Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até que seja válido). 
-O programa será encerrado quando o código informado for o número 4.*/
+/*Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. O último dado, 
+ não entrará nos cálculos, contém o valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos.*/
 public class uri1154{
     public static void main(String[] args) {
         
-        int op=0,alcool=0,gasolina=0,diesel=0;
+        int idade=0;
+        double media=0;
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        
+        int i=0;
+        while(idade >= 0){
+            idade = sc.nextInt();
+            
+            if(idade > 0){
+                
+                media+=idade;
+                i++;
+            }
 
+        }
+
+        //System.out.printf("%d\n",i);
+
+        media /=(double) i;
+
+        System.out.printf("%.2f\n",media);
 
         sc.close();
     }
