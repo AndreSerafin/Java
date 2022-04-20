38a5fd2cfa2390ad39fcb75882377267cc559881
AndreSerafin <andreserafim11@gmail.com>
2022-04-20 17:41:17 -0300

    vetores

--- ExerciciosAulas/src/exVetores009.java
+++ ExerciciosAulas/src/exVetores009.java
@@ -24,16 +24,17 @@ public class exVetores009 {
             precoDeVenda[i] = sc.nextDouble();
         }
 
-        double lucro = 0;
         double[] porcentagemLucro = new double[n];
-        int menorQue10 = 0;
-        int entre10e20 = 0;
-        int maiorQue20 = 0;
+        double lucro = 0,totalCompra = 0, totalVenda = 0, lucroTotal = 0;;
+        int menorQue10 = 0, entre10e20 = 0, maiorQue20 = 0;
 
         for(int i = 0; i < n; i++){
+           
             lucro = precoDeVenda[i] - precoDeCompra[i];
             porcentagemLucro[i] = (lucro/precoDeCompra[i])*100.0;
-            System.out.printf("Lucro = %.2f%%\n",porcentagemLucro[i]);
+            
+            //System.out.printf("Lucro = %.2f%%\n",porcentagemLucro[i]);
+
             if(porcentagemLucro[i] < 10.0){
                 menorQue10++;
             }else if(porcentagemLucro[i] <= 20.0){
@@ -41,11 +42,20 @@ public class exVetores009 {
             }else{
                 maiorQue20++;
             }
+
+            totalCompra += precoDeCompra[i];
+            totalVenda += precoDeVenda[i];
+            lucroTotal += lucro;
+
         }
+        
 
         System.out.printf("Lucro abaixo de 10%%: %d\n",menorQue10);
         System.out.printf("Lucro entre 10%% e 20%%: %d\n",entre10e20);
         System.out.printf("Lucro acima de 20%%: %d\n",maiorQue20);
+        System.out.printf("Valor total de Compra: %.2f\n",totalCompra);
+        System.out.printf("Valor total de Venda: %.2f\n",totalVenda);
+        System.out.printf("Lucro total: %.2f\n",lucroTotal);
 
 
         sc.close();
