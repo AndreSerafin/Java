15b63bd35c01eae6fd5769204617e0ccd39d1aed
AndreSerafin <andreserafim11@gmail.com>
2022-04-29 13:30:00 -0300

    matrizes

--- ExerciciosAulas/src/exMatrizes007.java
+++ ExerciciosAulas/src/exMatrizes007.java
@@ -11,6 +11,8 @@ public class exMatrizes007 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.printf("Digite o tamanho da matriz (N x M)\n");
+
         int n = sc.nextInt();
         int m = sc.nextInt();
         
@@ -21,6 +23,16 @@ public class exMatrizes007 {
                 a[i][j] = sc.nextInt();
             }
         }
+
+        System.out.println();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.printf("%d ",a[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
         int[][] b = new int[n][m];
         System.out.printf("Digite a linha que deseja girar(1 a %d)\n",n);
         int linha = sc.nextInt() - 1;
