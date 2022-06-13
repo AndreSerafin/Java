8284351c72c845194f234b7b4351deed6490fe62
Andre Serafin <andreserafim11@gmail.com>
2022-06-13 10:59:40 -0300

    ex13

---
+++ ProgramacaoOrientadaObjetos/IntroduçãoPOO/execicio13-ClienteDocumentos/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+/*Sabe-se que um documento só pode ser cadastrado para um cliente que já exista.Considere que podem
+existir,no máximo,15 clientese30 documentos.Crie um vetor para clienteseoutro para documentos.Crie
+um menu paraarealização de cada uma das operações especificadasaseguir.
+a)Cadastrar clientes não pode existir mais que um cliente comomesmo código.
+b)Cadastrar documentos ao cadastrar um documento,seadata de pagamento for maior queadata de venci
+mento,calcularocampo'juros'do registro documentos(5%sobreovalor original do documento).
+c)Excluir clientes-um cliente só poderá ser excluído se não existir nenhum documento associadoaele.
+d)Excluir documentos individuais-por meio de seu número.Casoodocumento não exista,oprograma deverá
+mostraramensagem Documento não encontrado.
+e)Excluir documentos por cliente-oprograma deverá informarocódigo do clienteeexcluir todos os seus
+documentos.Casoocliente não exista,deverá mostraramensagem Cliente não encontrado.
+f)Excluir documentos por período-oprograma deverá informaradata inicialeadata finaleexcluir todos os
+documentos que possuam data de vencimento nesse período.
+g)Alterar as informações sobre os clientes-só não pode ser alteradoocódigo do cliente.
+h)Mostrar o total de documentos de determinado cliente.
+i)Sair.*/
+
+public class Program {
+}
