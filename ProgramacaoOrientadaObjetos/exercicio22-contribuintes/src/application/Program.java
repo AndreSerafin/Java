bacb986d52dae2d35fa1141932e547faf0186016
Andre Serafin <andreserafim11@gmail.com>
2022-06-28 09:47:30 -0300

    22

---
+++ ProgramacaoOrientadaObjetos/exercicio22-contribuintes/src/application/Program.java
@@ -0,0 +1,70 @@
+package application;
+
+import entities.Contribuintes;
+import entities.PessoaFisica;
+import entities.PessoaJuridica;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Numero de contribuintes: ");
+        int n = sc.nextInt();
+
+        ArrayList<Contribuintes> contribuintes = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Contribuinte #%d\n", i + 1);
+            System.out.print("Individual ou Companhia(i/c)? ");
+            sc.nextLine();
+            char op = sc.next().charAt(0);
+            sc.nextLine();
+            System.out.print("Nome: ");
+            String nome = sc.nextLine();
+            System.out.print("Renda anual: ");
+            double renda = sc.nextDouble();
+            boolean key = false;
+            do{
+                switch (op) {
+                    case 'i' -> {
+                        System.out.print("Gastos com saúde: ");
+                        double gastos = sc.nextDouble();
+                        contribuintes.add(new PessoaFisica(nome, renda, gastos));
+                        key = true;
+                    }
+                    case 'c' -> {
+                        System.out.print("Numero de funcionarios: ");
+                        int numFuncionarios = sc.nextInt();
+                        contribuintes.add(new PessoaJuridica(nome, renda, numFuncionarios));
+                        key = true;
+                    }
+                    default -> {
+                        System.out.print("Opção Invalida!\nSelecione novamente (c/i): ");
+                        sc.nextLine();
+                        op = sc.next().charAt(0);
+                    }
+                }
+            }while (!key);
+        }
+
+        System.out.print("Taxas pagas: ");
+        for (Contribuintes c : contribuintes){
+            System.out.printf("\n%s: R$ %.2f", c.getNome(), c.calculoImposto());
+        }
+
+        double soma = 0.0;
+        for(Contribuintes c : contribuintes) {
+            soma += c.calculoImposto();
+        }
+        System.out.printf("\nTaxas Totais: R$ %.2f\n", soma);
+        
+        sc.close();
+    }
+}
---
+++ ProgramacaoOrientadaObjetos/exercicio22-contribuintes/src/entities/PessoaFisica.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class PessoaFisica extends Contribuintes{
+
+    Double gastosSaude;
+
+    public PessoaFisica() {
+        super();
+    }
+
+    public PessoaFisica(String nome, Double rendaAnual, Double gastosSaude) {
+        super(nome, rendaAnual);
+        this.gastosSaude = gastosSaude;
+    }
+
+    public Double getGastosSaude() {
+        return gastosSaude;
+    }
+
+    @Override
+    public Double calculoImposto() {
+        if (getRendaAnual() < 20000.0) {
+            return (getRendaAnual() * 0.15) - (gastosSaude * 0.5);
+        }
+        return (getRendaAnual() * 0.25) - (gastosSaude * 0.5);
+    }
+}
